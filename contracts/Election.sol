7e1afbe2cd6279435f82486c6ebc878e85d549a3
ZaHuPro <itsjakirhussain@gmail.com>
2020-01-25 22:58:52 +0530

    First Migration

---
+++ contracts/Election.sol
@@ -0,0 +1,13 @@
+pragma solidity >=0.4.21 <0.7.0;
+
+contract Election {
+    //Store Candidate
+    //Read Candidate
+        string public candidate;
+    //Constructor
+
+    constructor() public {
+        candidate = 'Cadidate 1';
+    }
+
+}
