e110e2072a08cb566c8724ae1cb7b8a921347110
ZaHuPro <itsjakirhussain@gmail.com>
2020-03-09 22:31:21 +0530

    Commit

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,13 +1,24 @@
 pragma solidity >=0.4.21 <0.7.0;
 
 contract Election {
-    //Store Candidate
-    //Read Candidate
-        string public candidate;
-    //Constructor
+
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    mapping(uint => Candidate) public candidates;
+    uint public candidatesCount;
 
     constructor() public {
-        candidate = 'Cadidate 1';
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate (string memory _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 
 }
